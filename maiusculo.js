fbc1ddb6bff7d823949a813b618e11652d6ac7ff
amvicktor <amvicktor@gmail.com>
2026-04-19 16:04:25 -0400

    Delete maiusculo.js

--- maiusculo.js
+++
@@ -1,8 +0,0 @@
-const transformarParaMaiusculas = (arrayDeNomes) => {
-  return arrayDeNomes.map(nome => nome.toUpperCase());
-};
-
-const nomes = ['vicktor', 'sofia', 'vick', 'sofi'];
-const nomesMaiusculos = transformarParaMaiusculas(nomes);
-
-console.log(nomesMaiusculos);
